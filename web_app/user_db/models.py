17dbc5b66ae5b81748eebd51838e4f1858168f3f
jordanenichols <jordan.jbns@gmail.com>
2019-10-22 13:27:50 -0400

    added extended user model, changed time zone

--- web_app/user_db/models.py
+++ web_app/user_db/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+@receiver(pos_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(pos_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    isntance,profile.save()
+
+
